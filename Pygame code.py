ec23103ffa593f00d7e7775647d7026513022fea
Martin Coffey <mcoffey1129@hotmail.com>
2021-07-06 15:10:33 +0100

    "Pygame - 1st save"

---
+++ Pygame code.py
@@ -0,0 +1,18 @@
+
+# Packages
+import numpy as np
+
+print("Hello World")
+
+# Assignment operators
+x = 10
+print(x) # value of 10
+
+x = x+1
+print(x) # value of 11
+
+x+=1
+print(x) # value of 12
+
+# Trig functions
+x = sin()
