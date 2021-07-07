5f522c56d66ce76796e756bea88c59cf24d2a217
Martin Coffey <mcoffey1129@hotmail.com>
2021-07-07 08:22:46 +0100

    "Pygame - 3rd save"

--- Pygame code.py
+++ Pygame code.py
@@ -177,3 +177,107 @@ PI = 3.1415926535
 # Open a window of width 700 pixels and height of 500
 size = (700, 500)
 screen = pygame.display.set_mode(size)
+
+##################################### Main program loop############################################################
+
+pygame.display.set_caption("Alien Attack")
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+font = pygame.font.Font(None, 25)
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
+
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    for x in range(0,100,20):
+        pygame.draw.line(screen, RED, [x, 0], [x, 100], 5)
+
+    score = 20
+    text = font.render("Score: " + str(score), True, BLACK)
+    screen.blit(text, [100, 100])
+
+    pygame.display.flip()
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 20 frames per second
+    clock.tick(20)
+
+################################################################################################################
+
+# Loops
+for i in range(10):
+    print("*", end =" ")
+
+for i in range(10):
+    print("*", end = " ")
+for j in range(5):
+    print("*", end = " ")
+for k in range(15):
+    print("*", end = " ")
+
+for i in range(10):
+    for j in range(10):
+        print("*", end = " ")
+    print()
+
+for i in range(10):
+    for j in range(5):
+        print("*", end = " ")
+    print()
+
+for i in range(5):
+    for j in range(20):
+        print("*", end = " ")
+    print()
+
+for i in range(10):
+    for j in range(10):
+        print(j, end=" ")
+    print()
+
+for i in range(10):
+    for j in range(10):
+        print(i, end=" ")
+    print()
+
+for i in range(10):
+    for j in range(i+1):
+        print(j, end=" ")
+    print()
+
+
+# Adding to lists
+my_list = [] # Empty list
+for i in range(5):
+    user_input = input( "Enter an integer: ")
+    user_input = int(user_input)
+    my_list.append(user_input)
+    print(my_list)
+
+# Secret code
+plain_text = "This is a test. ABC abc"
+for c in plain_text:
+    print(c, end=" ")
+
+
+plain_text = "This is a test. ABC abc"
+for c in plain_text:
+    print(ord(c), end=" ")
+
+
